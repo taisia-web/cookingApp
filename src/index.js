72e22af918f3f9b035a94d1bdad91481be82a827
LikerK <nagorniykirill@gmail.com>
2024-10-14 21:47:31 +0300

    add search and render recipes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './style.css';
+import RECIPES from './recipes.js';
+
+const input = document.querySelector('input');
+const recipes = document.querySelector('.recipes');
+
+const renderRecipes = (recipe) => {
+    recipe.forEach(i => {
+        const div = document.createElement('div');
+        div.classList.add('recipe');
+        div.textContent = i.name;
+        recipes.append(div);
+    });
+}
+
+input.addEventListener('input', (e) => {
+    recipes.innerHTML = '';
+    const search = e.target.value;
+    const data = RECIPES.filter(i => i.name.toLowerCase().startsWith(search));
+    renderRecipes(data)
+});
+
+renderRecipes(RECIPES);
