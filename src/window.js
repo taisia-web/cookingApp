a894df03e64c1e063f8512849979437d3d523c2c
taisia-web <aksenovataisia3@gmail.com>
2024-10-27 20:57:00 +0300

    correct ingredients and close modal with click on overlay

--- src/window.js
+++ src/window.js
@@ -3,17 +3,32 @@ const createModal = (recipe) => {
   const content = document.createElement('div');
   overlay.classList.add('modal-overlay');
   content.classList.add('modal-content');
+  
+  const headerModal = document.createElement('div');
+  headerModal.classList.add('header-modal');
   const closeButton = document.createElement('button');
   const span = document.createElement('span');
   span.textContent = recipe.name;
+
   closeButton.textContent = 'Close';
   closeButton.addEventListener('click', () => {
       overlay.remove();
   });
+  overlay.addEventListener('click', (e)=>{
+    console.log(e.target)
+    if (e.target.className === 'modal-overlay'){
+      overlay.remove()
+    }
+  })
+
+
+  headerModal.append(span);
+  headerModal.append(closeButton);
+  
+
   const ingredients = createIngredients(recipe.ingredients);
   overlay.append(content);
-  content.append(span);
-  content.append(closeButton);
+  content.append(headerModal);
   content.append(ingredients);
   return overlay;
 }
@@ -22,9 +37,10 @@ const createIngredients = (ingr) => {
   const ul = document.createElement('ul');
   ingr.forEach(i => {
     const li = document.createElement('li');
-    li.textContent = i;
+    li.textContent = i[0].toUpperCase()+i.slice(1);
     ul.append(li);
   })
+  ul.classList.add('list-ingr');
   return ul;
 }
 
