5615458585597aff0df22dc69a2a8c80229841cf
likerk <nagorniykirill@gmail.com>
2024-10-27 20:44:01 +0400

    edit window.js

--- src/window.js
+++ src/window.js
@@ -1,5 +1,4 @@
 const createModal = (recipe) => {
-<<<<<<< HEAD
   const overlay = document.createElement('div');
   const content = document.createElement('div');
   overlay.classList.add('modal-overlay');
@@ -20,34 +19,6 @@ const createModal = (recipe) => {
 }
 
 const createIngredients = (ingr) => {
-const ul = document.createElement('ul');
-ingr.forEach(i => {
-  const li = document.createElement('li');
-  li.textContent = i;
-  ul.append(li);
-})
-return ul;
-=======
-    const overlay = document.createElement('div');
-    const content = document.createElement('div');
-    overlay.classList.add('modal-overlay');
-    content.classList.add('modal-content');
-    const closeButton = document.createElement('button');
-    const span = document.createElement('span');
-    span.textContent = recipe.name;
-    closeButton.textContent = 'Close';
-    closeButton.addEventListener('click', () => {
-        overlay.remove();
-    });
-    const ingredients = createIngredients(recipe.ingredients);
-    overlay.append(content);
-    content.append(span);
-    content.append(closeButton);
-    content.append(ingredients);
-    return overlay;
-}
-
-const createIngredients = (ingr) => {
   const ul = document.createElement('ul');
   ingr.forEach(i => {
     const li = document.createElement('li');
@@ -55,7 +26,6 @@ const createIngredients = (ingr) => {
     ul.append(li);
   })
   return ul;
->>>>>>> refs/remotes/origin/main
 }
 
 export default createModal;
