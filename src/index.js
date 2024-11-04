fe15c0845b06c343e43f24d791a27be40e337aec
LikerK <nagorniykirill@gmail.com>
2024-11-04 14:17:11 +0300

    add search for recipes by meal

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import createModal from './window.js';
 const input = document.querySelector('input');
 const recipes = document.querySelector('.recipes');
 const body = document.querySelector('body');
+const nav = document.querySelector('nav');
 
 const renderRecipes = (recipesData) => {
+    recipes.innerHTML = '';
     recipesData.forEach(recipe => {
         const componentRecipe = createReceipe(recipe);
         recipes.append(componentRecipe);
@@ -18,11 +20,30 @@ const renderRecipes = (recipesData) => {
     });
 }
 
+const searchRecipes = (search, type=false) => {
+  if (type) {
+    const data = RECIPES.filter(i => i.type === search);
+    renderRecipes(data);
+  } else {
+    const data = RECIPES.filter(i => i.name.toLowerCase().startsWith(search.toLowerCase()));
+    renderRecipes(data);
+  }
+}
+
 input.addEventListener('input', (e) => {
     recipes.innerHTML = '';
     const search = e.target.value;
-    const data = RECIPES.filter(i => i.name.toLowerCase().startsWith(search.toLowerCase()));
-    renderRecipes(data)
+    searchRecipes(search);
 });
+
+
+nav.addEventListener('click', (e) => {
+  if (e.target.dataset.item === 'btn') {
+    console.log(e.target.dataset.type)
+    const data = e.target.dataset.type;
+    searchRecipes(data, true);
+  }
+})
+
 
 renderRecipes(RECIPES);
--- src/recipes.js
+++ src/recipes.js
@@ -1,11 +1,11 @@
 const RECIPES = [
-  {name: 'Жаренный крылья', ingredients: ['крылья', 'масло', 'специи'], image: 'https://images.allhungry.com/0/img/menu/wings/Wings_3.jpg'},
-  {name: 'Куриный суп', ingredients:['курица', 'вода', 'специи', 'лапша'], image: 'https://avatars.mds.yandex.net/get-entity_search/5488405/910059936/S600xU_2x'},
-  {name: 'Борщ', ingredients:['говядина', 'вода', 'специи', 'морковь', 'картошка', 'зелень'], image: 'https://avatars.mds.yandex.net/get-entity_search/5393828/924530106/S600xU_2x'},
-  {name: 'Ребра', ingredients:['свинина', 'соус острый', 'специи'], image: 'https://i.pinimg.com/originals/82/68/69/826869734a6637abf7efe5fc8aa8e11d.jpg'},
-  {name: 'Пельмени', ingredients:['пельмени', 'вода', 'специи'], image: 'https://sn-gazeta.ru/wp-content/uploads/2023/04/gsxlhwirnhbtyz8ckdjo.jpg'},
-  {name: 'Салат Цезарь', ingredients:['курица', 'салат', 'специи', 'помидоры черри', 'сухари', 'сыр', 'соус Цезарь'], image: 'https://media.vprok.ru/recipe/x956/vg/xu/wnerucdufxsyc72ngt2qhd7knj5gxuvg.jpeg'},
-  {name: 'Вода', ingredients:['вода'], image: 'https://i.pinimg.com/originals/08/51/0f/08510f22783fdbfcff925053a65554f9.jpg'},
+  {name: 'Жаренный крылья', type: 'dinner',  ingredients: ['крылья', 'масло', 'специи'], image: 'https://images.allhungry.com/0/img/menu/wings/Wings_3.jpg'},
+  {name: 'Куриный суп', type:'lunch', ingredients:['курица', 'вода', 'специи', 'лапша'], image: 'https://avatars.mds.yandex.net/get-entity_search/5488405/910059936/S600xU_2x'},
+  {name: 'Борщ', type:'lunch', ingredients:['говядина', 'вода', 'специи', 'морковь', 'картошка', 'зелень'], image: 'https://avatars.mds.yandex.net/get-entity_search/5393828/924530106/S600xU_2x'},
+  {name: 'Ребра', type:'lunch', ingredients:['свинина', 'соус острый', 'специи'], image: 'https://i.pinimg.com/originals/82/68/69/826869734a6637abf7efe5fc8aa8e11d.jpg'},
+  {name: 'Пельмени', type: 'dinner', ingredients:['пельмени', 'вода', 'специи'], image: 'https://sn-gazeta.ru/wp-content/uploads/2023/04/gsxlhwirnhbtyz8ckdjo.jpg'},
+  {name: 'Салат Цезарь', type: 'breakfast', ingredients:['курица', 'салат', 'специи', 'помидоры черри', 'сухари', 'сыр', 'соус Цезарь'], image: 'https://media.vprok.ru/recipe/x956/vg/xu/wnerucdufxsyc72ngt2qhd7knj5gxuvg.jpeg'},
+  {name: 'Вода', type: 'breakfast', ingredients:['вода'], image: 'https://i.pinimg.com/originals/08/51/0f/08510f22783fdbfcff925053a65554f9.jpg'},
 ]
 
 export default RECIPES;
